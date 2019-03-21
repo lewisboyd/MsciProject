fbcafa8ec158425d1b70c7265c051b8f1e5160e5
Lewis Boyd <lewis-boyd@live.co.uk>
2019-03-21 14:49:31 +0000

    Added script to manually play through environment using the keyboard and discrete actions

---
+++ retina_reinforcement_sim/scripts/baxter_play_env.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import sys
+import random
+import time
+
+import cv2
+import rospy
+
+from environment import BaxterEnvironment
+
+
+def run():
+    rospy.init_node("test")
+    env = BaxterEnvironment()
+    env.reset()
+    rospy.on_shutdown(env.close)
+    cv2.namedWindow("Feed", cv2.WINDOW_NORMAL)
+
+    # Key action mapping
+    actions = {ord('a'): [0.1, 0], ord('s'): [-0.1, 0],
+               ord('d'): [0, 0.1], ord('f'): [0, -0.1]}
+
+    # Run until interupted or 'esc' pressed
+    done = True
+    key = None
+    while not rospy.is_shutdown():
+        if done:
+            # Reset environment
+            state = env.reset()
+            if verbose:
+                print ""
+                print "horiz: " + str(state[0])
+                print "vert: " + str(state[1])
+            done = False
+        else:
+            if key in actions:
+                # If valid key pressed execute corresponding action
+                action = actions[key]
+                state, reward, done = env.step(action)
+                print ""
+                print "action: " + str(action)
+                print "horiz: " + str(state[0])
+                print "vert: " + str(state[1])
+                print "reward: " + str(reward)
+                print "done: " + str(done)
+
+        # Display image until key pressed
+        cv2.imshow("Feed", env.image)
+        key = cv2.waitKey(waitTime)
+
+        # If 'esc' pressed exit program
+        if key == 27:
+            break;
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(run())
+    except rospy.ROSInterruptException:
+        pass
