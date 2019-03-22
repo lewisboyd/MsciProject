76a334265afd1f26e416727519e1cf0e9d433448
Lewis Boyd <lewis-boyd@live.co.uk>
2019-03-22 17:16:28 +0000

    Added Actor Critic models using Selu instead of Relu + linear norm

--- retina_reinforcement_sim/src/model/models.py
+++ retina_reinforcement_sim/src/model/models.py
@@ -311,3 +311,95 @@ class CriticMlp(nn.Module):
 
         x = self.fc3(x)
         return x
+
+class ActorSnn(nn.Module):
+    """Represents an Actor in the Actor to Critic Model."""
+
+    def __init__(self, state_dim, action_dim):
+        """Initialise layers.
+
+        Args:
+            state_dim (int) : Number of state inputs
+            action_dim (int) : Number of action ouputs
+        """
+        # Create network
+        super(ActorSnn, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 400)
+        self.fc2 = nn.Linear(400, 300)
+        self.fc3 = nn.Linear(300, action_dim)
+
+        # Initialise weights
+        selu_weights_init(self.fc1)
+        selu_weights_init(self.fc2)
+        selu_weights_init(self.fc3)
+        nn.init.uniform_(self.fc3.weight, -0.0003, 0.0003)
+
+    def forward(self, batch):
+        """Generate action policy for the batch of states.
+
+        Args:
+            batch (float tensor) : Batch of states
+
+        Returns:
+            float tensor : Batch of action policies
+
+        """
+        x = self.fc1(batch)
+        x = F.selu(x)
+
+        x = self.fc2(x)
+        x = F.selu(x)
+
+        x = self.fc3(x).tanh()
+        return x
+
+
+class CriticSnn(nn.Module):
+    """Represents a Critic in the Actor to Critic Model."""
+
+    def __init__(self, state_dim, action_dim):
+        """Initialise layers.
+
+        Args:
+            state_dim (int) : Number of state inputs
+            action_dim (int) : Number of action ouputs
+
+        """
+        # Create network
+        super(CriticSnn, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 400)
+        self.fc2 = nn.Linear(400 + action_dim, 300)
+        self.fc3 = nn.Linear(300, 1)
+
+        # Initialise weights
+        selu_weights_init(self.fc1)
+        selu_weights_init(self.fc2)
+        selu_weights_init(self.fc3)
+        nn.init.uniform_(self.fc3.weight, -0.0003, 0.0003)
+
+    def forward(self, state_batch, action_batch):
+        """Generate Q-values for the batch of states and actions pairs.
+
+        Args:
+            state_batch (float tensor) : Batch of states
+            action_batch (float tensor) : Batch of actions
+
+        Returns:
+            float tensor : Batch of Q-values
+
+        """
+        x = self.fc1(state_batch)
+        x = F.selu(x)
+        x = torch.cat((x, action_batch), 1)
+
+        x = self.fc2(x)
+        x = F.selu(x)
+
+        x = self.fc3(x)
+        return x
+
+def selu_weights_init(m):
+    size = m.weight.size()
+    fan_in = size[1]
+    m.weight.data.normal_(0.0, 1.0 / math.sqrt(fan_in))
+    m.bias.data.fill_(0)
