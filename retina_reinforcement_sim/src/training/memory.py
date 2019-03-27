22e77bfbb39e48dc4e8d190f4d63f144a0d7f114
Lewis Boyd <lewis-boyd@live.co.uk>
2019-03-27 13:56:05 +0000

    Added DDPG HER agent

--- retina_reinforcement_sim/src/training/memory.py
+++ retina_reinforcement_sim/src/training/memory.py
@@ -60,3 +60,64 @@ class ReplayMemory(object):
     def __len__(self):
         """Return the length of the ReplayMemory."""
         return len(self.memory)
+
+
+class HerMemory(object):
+    """Class to store and sample experiences with goals during training."""
+
+    Experience = namedtuple('Experience',
+                            ('state', 'action', 'next_state', 'reward',
+                             'done'))
+
+    def __init__(self, capacity):
+        """Create empty list bounded by the capacity.
+
+        Args:
+            capacity (int): Maximum number of experiences to store
+
+        """
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, states, actions, next_states, rewards, dones, goal):
+        """Save an episode of experiences with desired goal.
+
+        Args:
+            state (tensor): The states the action executed in
+            action (tensor): The actions values
+            next_state (tensor): Next states after actions executed
+            reward (tensor): Rewards given for executing the actions
+            done (tensor): 0 for episode finished, 1 for still in progress
+            goal (tensor): Goal state
+
+        """
+        for i in range(len(states)):
+            state = torch.cat([states[i], goal])
+            action = actions[i]
+            next_state = torch.cat([next_states[i], goal])
+            reward = rewards[i]
+            done = dones[i]
+            if len(self.memory) < self.capacity:
+                experience = ReplayMemory.Experience(
+                    state, action, next_state, reward, done)
+                self.memory.append(experience)
+            else:
+                # Use copy_ instead of creating new experience due to pytorch
+                # not correctly freeing the GPU memory (5x slower but no leak)
+                self.memory[self.position].state.copy_(state)
+                self.memory[self.position].action.copy_(action)
+                self.memory[self.position].next_state.copy_(next_state)
+                self.memory[self.position].reward.copy_(reward)
+                self.memory[self.position].done.copy_(done)
+                self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        """
+        Returns batch_size number of samples from the replay memory
+        """
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        """Return the length of the ReplayMemory."""
+        return len(self.memory)
