d1fc4dce665707eaeb6aa9faff521cec4aa2e01c
Lewis Boyd <lewis-boyd@live.co.uk>
2019-04-05 18:21:59 +0100

    Added 10 layer ResNet using full preactivtion

--- retina_reinforcement_sim/src/model/feature_extractor.py
+++ retina_reinforcement_sim/src/model/feature_extractor.py
@@ -45,3 +45,90 @@ class FeatureExtractor(nn.Module):
         x = self.relu(x)
 
         return self.fc1(x).tanh()
+
+
+class BasicBlock(nn.Module):
+    """2 layer full preactivion ResNet block."""
+
+    def __init__(self, inplanes, outplanes, stride):
+        """Initialise layers.
+
+        Args:
+            inplanes (int): Number of inplanes.
+            outplanes (int): Desired number of outplanes.
+            stride (int): Stride for first conv layer.
+        """
+        super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(inplanes, outplanes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(outplanes)
+        self.conv2 = nn.Conv2d(outplanes, outplanes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.downsample = nn.Sequential()
+        if stride != 1:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(inplanes, outplanes, kernel_size=1, stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(outplanes)
+            )
+
+    def forward(self, x):
+        """Process input tensor."""
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.conv1(out)
+
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+
+        out += self.downsample(x)
+
+        return out
+
+
+class ResNet10(nn.Module):
+    """10 Layer ResNet (4.9M params) using full preactivion."""
+
+    def __init__(self, state_dim):
+        """Initialise layers.
+
+        Args:
+            state_dim (int): Number of values to regress
+        """
+        # Create network
+        super(ResNet10, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.block1 = BasicBlock(64, 64, 1)
+        self.block2 = BasicBlock(64, 128, 2)
+        self.block3 = BasicBlock(128, 256, 2)
+        self.block4 = BasicBlock(256, 512, 2)
+        self.bn1 = nn.BatchNorm2d(512)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc1 = nn.Linear(512, state_dim)
+
+    def forward(self, batch):
+        """Generate predictions for batch of images.
+
+        Args:
+            batch (float tensor): Batch of images.
+
+        Returns:
+            float tensor : Batch of predictions.
+        """
+        x = self.conv1(batch)
+
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return self.fc1(x).tanh()
