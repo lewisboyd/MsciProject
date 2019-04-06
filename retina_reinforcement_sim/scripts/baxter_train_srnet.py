70f1099f5c2b1ae04cc6dd868eb305fe70bc2e9e
Lewis Boyd <lewis-boyd@live.co.uk>
2019-04-06 17:42:35 +0100

    Finished training code, reduced size of dataset from 100000 to 50000

--- retina_reinforcement_sim/scripts/baxter_train_srnet.py
+++ retina_reinforcement_sim/scripts/baxter_train_srnet.py
@@ -6,13 +6,14 @@ import argparse
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import Dataset, DataLoader
-import matplotlib.pyplot as plt
 
-from model import FeatureExtractorNorm
+from model import ResNet10
 
 
 class ImageStateDataset(Dataset):
@@ -31,7 +32,7 @@ class ImageStateDataset(Dataset):
             self.img_dir = self.base_dir + "retina_images/"
         else:
             self.img_dir = self.base_dir + "images/"
-        states = torch.load(self.base_dir + "states")
+        states = torch.load(self.base_dir + "states")[:50000]
         split = states.size(0) - (states.size(0) / 10)
         if is_test:
             self.states = states[split:]
@@ -45,10 +46,10 @@ class ImageStateDataset(Dataset):
     def __getitem__(self, id):
         """Index dataset to get image and state pair."""
         img_name = self.img_dir + "img" + str(id) + ".png"
-        img = cv2.imread(img_name)
+        img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
         # Convert to float and rescale to [0,1]
         img = img.astype(np.float32) / 255
-        img = torch.tensor(img, dtype=torch.float).permute(2, 0, 1)
+        img = torch.tensor(img, dtype=torch.float).unsqueeze(0)
         state = self.states[id]
         return(img, state)
 
@@ -61,14 +62,17 @@ def str2bool(value):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Train State Representation Net.')
-    parser.add_argument('--use-retina', type=str2bool, required=True,
+    parser.add_argument('--use-retina', type=str2bool, default=False,
                         help='if true trains using retina images')
     args = parser.parse_args()
 
+    # Set training seed
+    torch.manual_seed(0)
+
     # Training variables
     use_retina = False
-    max_epoch = 100
-    batch_size = 32
+    max_epoch = 40
+    batch_size = 16
     criterion = nn.MSELoss()
     SAVE_FREQ = 5
     RESULTS_FOLDER = (os.path.dirname(
@@ -82,11 +86,11 @@ if __name__ == '__main__':
         RESULTS_FOLDER = RESULTS_FOLDER + "sr/results/"
         MODEL_FOLDER = MODEL_FOLDER + "sr/state_dicts/"
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    if args.use_retina:
-        net = FeatureExtractorNorm(3, 2).to(device)
-    else:
-        net = FeatureExtractorNorm(3, 2).to(device)
-    optimiser = optim.Adam(net.parameters())
+    net = ResNet10(2).to(device)
+    optimiser = optim.SGD(net.parameters(), lr=0.1, momentum=0.9,
+                          weight_decay=0.0001)
+    scheduler = ReduceLROnPlateau(optimiser, 'min', factor=0.1, patience=5,
+                                  min_lr=0.0001)
 
     # Create folders if don't exist
     if not os.path.isdir(RESULTS_FOLDER):
@@ -97,72 +101,76 @@ if __name__ == '__main__':
     # Create train and test dataloaders
     train_ds = ImageStateDataset(False, args.use_retina)
     train_dl = DataLoader(train_ds, batch_size, shuffle=True, pin_memory=True,
-                          num_workers=4)
+                          num_workers=1)
     test_ds = ImageStateDataset(True, args.use_retina)
-    test_dl = DataLoader(test_ds, batch_size, pin_memory=True, num_workers=4)
+    test_dl = DataLoader(test_ds, batch_size, pin_memory=True, num_workers=1)
 
     start = time.time()
 
-    # try:
-    epoch_train_losses = []
-    epoch_test_losses = []
-    for epoch in range(1, max_epoch + 1):
-        # Train
-        train_losses = []
-        for i, batch in enumerate(train_dl):
-            images, targets = batch
-            print images.size()
-            print targets.size()
-            images = images.to(device)
-            targets = targets.to(device)
-            optimiser.zero_grad()
-            preds = net(images)
-            loss = criterion(preds, targets)
-            loss.backward()
-            optimiser.step()
-            train_losses.append(loss.detach().cpu().numpy())
-
-        # Test
-        test_losses = []
-        with torch.no_grad():
-            for i, batch in enumerate(test_dl):
+    try:
+        epoch_train_losses = []
+        epoch_test_losses = []
+        for epoch in range(1, max_epoch + 1):
+            # Train
+            train_losses = []
+            for i, batch in enumerate(train_dl):
+                net = net.train()
                 images, targets = batch
                 images = images.to(device)
                 targets = targets.to(device)
+                optimiser.zero_grad()
                 preds = net(images)
                 loss = criterion(preds, targets)
-                test_losses.append(loss.detach().cpu().numpy())
+                loss.backward()
+                optimiser.step()
+                train_losses.append(loss.detach().cpu().numpy())
 
-        # Report performance
-        train_loss = np.asarray(train_losses).mean()
-        test_loss = np.asarray(test_losses).mean()
-        print "Epoch: %3d/%3d  Train Loss: %0.4f Test Loss: %0.4f" % (
-            epoch, max_epoch, train_loss, test_loss)
+            # Test
+            test_losses = []
+            with torch.no_grad():
+                for i, batch in enumerate(test_dl):
+                    net = net.eval()
+                    images, targets = batch
+                    images = images.to(device)
+                    targets = targets.to(device)
+                    preds = net(images)
+                    loss = criterion(preds, targets)
+                    test_losses.append(loss.detach().cpu().numpy())
 
-        epoch_train_losses.append(train_loss)
-        epoch_test_losses.append(test_loss)
+            # Report performance
+            train_loss = np.asarray(train_losses).mean()
+            test_loss = np.asarray(test_losses).mean()
+            print "Epoch: %3d/%3d  Train Loss: %0.4f Test Loss: %0.4f" % (
+                epoch, max_epoch, train_loss, test_loss)
+
+            # Update scheduler
+            scheduler.step(test_loss)
+
+            # Add train and test losses
+            epoch_train_losses.append(train_loss)
+            epoch_test_losses.append(test_loss)
+
+            # Save model
+            if (epoch % SAVE_FREQ == 0 and not epoch == max_epoch):
+                torch.save(net.state_dict(), MODEL_FOLDER
+                           + "net_" + str(epoch))
+    finally:
+        # Plot training results
+        plt.figure()
+        plt.plot(range(1, max_epoch + 1), epoch_train_losses)
+        plt.plot(range(1, max_epoch + 1), epoch_test_losses)
+        plt.legend(['Train loss', 'Test Loss'], loc='upper right')
+
+        # Save results
+        plt.savefig(RESULTS_FOLDER + "training_performance.png")
+        np.save(RESULTS_FOLDER + "train_loss", epoch_train_losses)
+        np.save(RESULTS_FOLDER + "test_loss", epoch_test_losses)
 
         # Save model
-        if (epoch % SAVE_FREQ == 0 and not epoch == max_epoch):
-            torch.save(net.state_dict(), MODEL_FOLDER
-                       + "net_" + str(epoch))
-    # finally:
-    #     # Plot training results
-    #     plt.figure()
-    #     plt.plot(range(1, max_epoch + 1), epoch_train_losses)
-    #     plt.plot(range(1, max_epoch + 1), epoch_test_losses)
-    #     plt.legend(['Train loss', 'Test Loss'], loc='upper right')
-    #
-    #     # Save results
-    #     plt.savefig(RESULTS_FOLDER + "training_performance.png")
-    #     np.save(RESULTS_FOLDER + "train_loss", epoch_train_losses)
-    #     np.save(RESULTS_FOLDER + "test_loss", epoch_test_losses)
-    #
-    #     # Save model
-    #     torch.save(net.state_dict(), MODEL_FOLDER
-    #                + "net_" + str(epoch))
-        #
-        # end = time.time()
-        # mins = (end - start) / 60
-        # print "Training finished after %d hours %d minutes" % (
-        #     mins / 60, mins % 60)
+        torch.save(net.state_dict(), MODEL_FOLDER
+                   + "net_" + str(epoch))
+
+        end = time.time()
+        mins = (end - start) / 60
+        print "Training finished after %d hours %d minutes" % (
+            mins / 60, mins % 60)
